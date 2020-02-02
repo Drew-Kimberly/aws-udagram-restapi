fd3ea4843b240e2a4506e1fee1087e088ca43394
Drew Kimberly <andrew.e.kimberly@gmail.com>
2020-02-02 17:05:38 -0500

    Configure codebase for development

--- src/aws.ts
+++ src/aws.ts
@@ -1,17 +1,18 @@
-import AWS = require('aws-sdk');
+import * as AWS from 'aws-sdk';
 import { config } from './config/config';
 
 // Configure AWS
 if (config.aws.aws_profile !== 'DEPLOYED') {
-    AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: config.aws.aws_profile});
+  AWS.config.credentials = new AWS.SharedIniFileCredentials({
+    profile: config.aws.aws_profile
+  });
 }
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: config.aws.aws_region,
-  params: {Bucket: config.aws.aws_media_bucket}
+  params: { Bucket: config.aws.aws_media_bucket }
 });
-
 
 /* getGetSignedUrl generates an aws signed url to retreive an item
  * @Params
@@ -19,15 +20,14 @@ export const s3 = new AWS.S3({
  * @Returns:
  *    a url as a string
  */
-export function getGetSignedUrl( key: string ): string {
-
+export function getGetSignedUrl(key: string): string {
   const signedUrlExpireSeconds = 60 * 5;
 
-    return s3.getSignedUrl('getObject', {
-        Bucket: config.aws.aws_media_bucket,
-        Key: key,
-        Expires: signedUrlExpireSeconds
-      });
+  return s3.getSignedUrl('getObject', {
+    Bucket: config.aws.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds
+  });
 }
 
 /* getPutSignedUrl generates an aws signed url to put an item
@@ -36,13 +36,12 @@ export function getGetSignedUrl( key: string ): string {
  * @Returns:
  *    a url as a string
  */
-export function getPutSignedUrl( key: string ) {
+export function getPutSignedUrl(key: string) {
+  const signedUrlExpireSeconds = 60 * 5;
 
-    const signedUrlExpireSeconds = 60 * 5;
-
-    return s3.getSignedUrl('putObject', {
-      Bucket: config.aws.aws_media_bucket,
-      Key: key,
-      Expires: signedUrlExpireSeconds
-    });
+  return s3.getSignedUrl('putObject', {
+    Bucket: config.aws.aws_media_bucket,
+    Key: key,
+    Expires: signedUrlExpireSeconds
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
 import { sequelize } from './sequelize';
-
 import { IndexRouter } from './controllers/v0/index.router';
-
 import bodyParser from 'body-parser';
-
 import { V0MODELS } from './controllers/v0/model.index';
 
 (async () => {
@@ -16,24 +14,40 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  // CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
+  // Setup CORS middleware.
+  const originWhitelist = [
+    'http://localhost:8100',
+    'http://d3puk6p5a7fcax.cloudfront.net'
+  ];
+  const corsOptions: CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization'
+    ],
+    origin: (
+      origin: string,
+      callback: (err: Error | null, allow?: boolean) => void
+    ) =>
+      originWhitelist.includes(origin)
+        ? callback(null, true)
+        : callback(null, false)
+  };
+  app.use(cors(corsOptions));
+  app.options('*', cors());
 
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
-
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
